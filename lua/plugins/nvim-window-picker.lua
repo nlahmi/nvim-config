45a05bb81e46620ccc8e9ef833d6e8f863baf89a
nlahmi <mogobekl123@gmail.com>
2024-02-24 14:01:33 +0200

    fixed return

--- lua/plugins/nvim-window-picker.lua
+++ lua/plugins/nvim-window-picker.lua
@@ -1,9 +1,9 @@
-{
-    's1n7ax/nvim-window-picker',
-    name = 'window-picker',
-    event = 'VeryLazy',
-    version = '2.*',
-    config = function()
-        require'window-picker'.setup()
-    end,
+return {
+  "s1n7ax/nvim-window-picker",
+  name = "window-picker",
+  event = "VeryLazy",
+  version = "2.*",
+  config = function()
+    require("window-picker").setup()
+  end,
 }
