dccbc3bf3902aa723253c48d2325b88958a90b78
nlahmi <mogobekl123@gmail.com>
2024-02-26 02:09:09 +0200

    Seperated UI and git do subdirs

---
+++ lua/plugins/init.lua
@@ -0,0 +1,5 @@
+return {
+  { import = "plugins.ui" },
+  { import = "plugins.lsp" },
+  { import = "plugins.git" },
+}
--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -1,4 +1,4 @@
-return {
+return
   {
     -- Source: https://www.lazyvim.org/plugins/lsp#masonnvim-1
     "williamboman/mason.nvim",
@@ -44,28 +44,4 @@ return {
     opts = {
       auto_install = true,
     },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    lazy = false,
-    config = function()
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
-
-      local lspconfig = require("lspconfig")
-      lspconfig.tsserver.setup({
-        capabilities = capabilities
-      })
-      lspconfig.html.setup({
-        capabilities = capabilities
-      })
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities
-      })
-
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-    end,
-  },
-}
+  }
