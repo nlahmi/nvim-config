657b0e531dd6b6ec036016ae5f425787f1520412
nlahmi <mogobekl123@gmail.com>
2024-02-29 01:50:32 +0200

    added keybind to switch sessions

--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -1,9 +1,22 @@
 return {
-  'rmagatti/auto-session',
-  config = function()
-    require("auto-session").setup {
-      log_level = "error",
-      auto_session_suppress_dirs = { "~/", "D:/Code", "~/Downloads", "/"},
-    }
-  end
+    "rmagatti/auto-session",
+    -- keys = { {"<leader>qs", require("auto-session.session-lens").search_session, desc = "Switch Session"} },
+    config = function()
+        require("auto-session").setup({
+            log_level = "error",
+            auto_session_suppress_dirs = { "~/", "D:/Code", "~/Downloads", "/" },
+            session_lens = {
+                -- If load_on_setup is set to false, one needs to eventually call `require("auto-session").setup_session_lens()` if they want to use session-lens.
+                buftypes_to_ignore = {}, -- list of buffer types what should not be deleted from current session
+                load_on_setup = true,
+                theme_conf = { border = true },
+                -- previewer = true,
+                previewer = false,
+            },
+        })
+        vim.keymap.set("n", "<leader>qs", require("auto-session.session-lens").search_session, {
+            noremap = true,
+            desc = "Switch Session",
+        })
+    end,
 }
