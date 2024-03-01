b076dc2a58d06559393ea6e6a0739bc7b0bc3eef
nlahmi <mogobekl123@gmail.com>
2024-03-02 00:57:31 +0200

    Added fugitive keybinds

--- lua/plugins/git/fugitive.lua
+++ lua/plugins/git/fugitive.lua
@@ -1,3 +1,25 @@
+local function add_commit_push(commit_msg)
+  vim.cmd(':Git add * | :Git commit -m "' .. commit_msg .. '" | :Git push')
+end
+
+local function ask_commit_msg()
+  vim.ui.input({ prompt = "Enter commit message: " }, add_commit_push)
+end
+
 return {
-    "tpope/vim-fugitive"
+  "tpope/vim-fugitive",
+  keys = {
+    { "<leader>ga", "<cmd>Git add *<cr>", desc = "Add *" },
+    { "<leader>gb", "<cmd>GBrowse<cr>", desc = "Browse" },
+    { "<leader>gc", "<cmd>Git commit<cr>i", desc = "Commit" },
+    { "<leader>gd", "<cmd>Gvdiffsplit<cr>", desc = "Diff (Split)" },
+    { "<leader>gD", "<cmd>Git diff<cr>", desc = "Diff" },
+    { "<leader>ge", "<cmd>Gvsplit<cr>", desc = "Edit (Split)" },
+    { "<leader>gE", "<cmd>Gedit<cr>", desc = "Edit" },
+    { "<leader>gg", "<cmd>Git<cr>", desc = "Summary" },
+    { "<leader>gk", ask_commit_msg, desc = "Commit and Push" },
+    { "<leader>gK", function() add_commit_push("auto commit") end, desc = "Auto Commit" },
+    { "<leader>gl", "<cmd>Git log<cr>", desc = "Log" },
+    { "<leader>gp", "<cmd>Git push<cr>", desc = "Push" },
+  },
 }
--- lua/plugins/git/gitsigns.lua
+++ lua/plugins/git/gitsigns.lua
@@ -3,7 +3,7 @@ return {
     config = function()
       require("gitsigns").setup()
 
-      vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", { desc = "Preview Hunk" })
+      vim.keymap.set("n", "<leader>gh", ":Gitsigns preview_hunk<CR>", { desc = "Preview Hunk" })
       vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", { desc = "Toggle Line Blame" })
     end
 }
