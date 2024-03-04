d49062a91f9cb49c07a6ccf4cea145fd31ec24ad
n.lahmi <noaml7778@gmail.com>
2024-03-04 19:48:44 +0200

    Added mini plugins

---
+++ lua/plugins/mini.lua
@@ -0,0 +1,13 @@
+return {
+    "echasnovski/mini.nvim",
+    version = false,
+    config = function()
+        require("mini.bracketed").setup()
+        require("mini.bufremove").setup()
+        require("mini.misc").setup()  -- Todo: add keybindings for utils in here
+        require("mini.splitjoin").setup()
+        require("mini.pairs").setup()
+
+        vim.keymap.set("n", "<leader>bd", require("mini.bufremove").delete, { desc = "Delete Buffer (Preserve Layout)" })
+    end,
+}
