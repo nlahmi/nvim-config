5110c832c3962ed1ccac3e32260d1aeaa5eb85a2
n.lahmi <noaml7778@gmail.com>
2024-03-11 01:34:07 +0200

    format

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,16 +1,16 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        auto_install = true,
-        highlight = { enable = true },
-        indent = { enable = true },
-      })
-    end
-  },
     {
-        "nvim-treesitter/nvim-treesitter-context"
-    }
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                auto_install = true,
+                highlight = { enable = true },
+                indent = { enable = true },
+            })
+        end,
+    },
+    {
+        "nvim-treesitter/nvim-treesitter-context",
+    },
 }
