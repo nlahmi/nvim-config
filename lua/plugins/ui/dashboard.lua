ea3311c9ca8463d4b11d73535d8e1fed839e36c9
n.lahmi <noaml7778@gmail.com>
2024-03-14 18:59:23 +0200

    auto commit

--- lua/plugins/ui/dashboard.lua
+++ lua/plugins/ui/dashboard.lua
@@ -1,11 +1,28 @@
 return {
-    "nvimdev/dashboard-nvim",
-    event = "VimEnter",
-    config = function()
-        require("dashboard").setup({
-            -- config
-        })
-    end,
-    dependencies = { { "nvim-tree/nvim-web-devicons" } },
-    keys = { { "<leader>pd", "<cmd>Dashboard<cr>", desc = "Dashboard" } },
+  "nvimdev/dashboard-nvim",
+  event = "VimEnter",
+  config = function()
+    require("dashboard").setup({
+      hide = {
+        -- this is taken care of by lualine
+        -- enabling this messes up the actual laststatus setting after loading a file
+        statusline = false,
+        tabline = false,
+        winbar = false,
+      },
+      shortcut_type = "number",
+      config = {
+        project = {
+          limit = 8,
+          enable = true,
+          icon = "󰏓 ",
+          icon_hl = "DashboardRecentProjectIcon",
+          action = "Telescope projects",
+          label = " Recent Projects:",
+        },
+      },
+    })
+  end,
+  dependencies = { { "nvim-tree/nvim-web-devicons" } },
+  keys = { { "<leader>pd", "<cmd>Dashboard<cr>", desc = "Dashboard" } },
 }
