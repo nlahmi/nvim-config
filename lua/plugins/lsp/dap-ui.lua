94bb31e35e7bc6a11d263c5ae3a8ee7094031632
nlahmi <mogobekl123@gmail.com>
2024-03-14 22:07:16 +0200

    Added reload .vscode and disabled dap-ui auto closing

--- lua/plugins/lsp/dap-ui.lua
+++ lua/plugins/lsp/dap-ui.lua
@@ -22,8 +22,8 @@ return {
     -- dap.listeners.before.event_terminated["dapui_config"] = function()
     --   dapui.close({})
     -- end
-    dap.listeners.before.event_exited["dapui_config"] = function()
-      dapui.close({})
-    end
+    -- dap.listeners.before.event_exited["dapui_config"] = function()
+    --   dapui.close({})
+    -- end
   end,
 }
