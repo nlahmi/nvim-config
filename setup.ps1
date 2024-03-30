ba1be4a013098ccc7e17d9d7c5124b612d8011fa
n.lahmi <noaml7778@gmail.com>
2024-03-30 23:20:16 +0300

    Changed to Hack font and updated setp.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,16 +1,19 @@
+#$chosen_font = "LiterationMono Nerd Font"
+$chosen_font = "Hack Nerd Font Mono"
+
 # Install Chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-# Install Nerd Font (LiterationMono)
-# https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/LiberationMono.zip
+# Install Nerd Font(s)
 $fonts = (New-Object -ComObject Shell.Application).Namespace(0x14)
 Get-ChildItem -Recurse -include *.ttf | % { $fonts.CopyHere($_.fullname) }
 
 # Set it as option in legacy CMD
-New-ItemProperty -LiteralPath "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Console\TrueTypeFont" -name 7778 -PropertyType String -Value "LiterationMono Nerd Font"
+New-ItemProperty -LiteralPath "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Console\TrueTypeFont" -name 7778 -PropertyType String -Value $chosen_font
+Set-ItemProperty -LiteralPath "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Console\TrueTypeFont" -name 7778 -PropertyType String -Value $chosen_font
 
 # Set it as default on legacy CMD
-Set-ItemProperty -LiteralPath "HKCU:\Console" -name "FaceName" -Value "LiterationMono Nerd Font Mono"
+Set-ItemProperty -LiteralPath "HKCU:\Console" -name "FaceName" -Value $chosen_font
 
 # Set it as default on Windows Terminal
 $p = $env:localappdata + "\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
@@ -23,14 +26,16 @@ if (Test-Path $p) {
     $decoded = Get-Content $p -Raw | ConvertFrom-Json
     
     # Add the font setting to the default profile
-    Add-Member -InputObject $decoded.profiles.defaults -NotePropertyName font -NotePropertyValue @{face="LiterationMono Nerd Font Mono"} -ErrorAction Ignore
+    Add-Member -InputObject $decoded.profiles.defaults -NotePropertyName font -NotePropertyValue @{face=$chosen_font} -ErrorAction Ignore
     
     # Save the file
     $decoded | ConvertTo-Json -Depth 100 -Compress | Set-Content $p
 }
 
+# TODO: Install gruvbox theme (for terminal and maybe others)
+
 # Install deps
-choco install lazygit mingw ripgrep fd git neovim neovide -y
+choco install wezterm lazygit mingw ripgrep fd git neovim neovide -y
 
 # Install lazy-nvim
 git clone https://github.com/nlahmi/nvim-config $env:LOCALAPPDATA\nvim
