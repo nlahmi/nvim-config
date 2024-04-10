9cee35df793ab23230e96d2c95d6bb76b6ee435f
n.lahmi <noaml7778@gmail.com>
2024-04-10 18:51:01 +0300

    dont remember lol

--- lua/plugins/lsp/lang/python.lua
+++ lua/plugins/lsp/lang/python.lua
@@ -34,6 +34,7 @@ return {
       "mfussenegger/nvim-dap-python",
       -- dependencies = { "linux-cultist/venv-selector.nvim" },
       -- stylua: ignore
+      ft = { "py" },
       keys = {
         { "<leader>dPt", function() require('dap-python').test_method() end, desc = "Debug Method", ft = "python" },
         { "<leader>dPc", function() require('dap-python').test_class() end,  desc = "Debug Class",  ft = "python" },
