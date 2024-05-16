af8b82961c5a17e707796a1e1d2425513f42830f
nlahmi <mogobekl123@gmail.com>
2024-05-16 22:55:07 +0300

    Updated noice

--- lua/plugins/ui/noice.lua
+++ lua/plugins/ui/noice.lua
@@ -1,6 +1,14 @@
 return
 {
   "folke/noice.nvim",
+  dependencies = {
+    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+    "MunifTanjim/nui.nvim",
+    -- OPTIONAL:
+    --   `nvim-notify` is only needed, if you want to use the notification view.
+    --   If not available, we use `mini` as the fallback
+    "rcarriga/nvim-notify",
+  },
   event = "VeryLazy",
   opts = {
     lsp = {
@@ -32,12 +40,12 @@ return
   },
   -- stylua: ignore
   keys = {
-    { "<S-Enter>", function() require("noice").redirect(vim.fn.getcmdline()) end, mode = "c", desc = "Redirect Cmdline" },
-    { "<leader>pnl", function() require("noice").cmd("last") end, desc = "Noice Last Message" },
-    { "<leader>pnh", function() require("noice").cmd("history") end, desc = "Noice History" },
-    { "<leader>pna", function() require("noice").cmd("all") end, desc = "Noice All" },
-    { "<leader>pnd", function() require("noice").cmd("dismiss") end, desc = "Dismiss All" },
-    { "<c-f>", function() if not require("noice.lsp").scroll(4) then return "<c-f>" end end, silent = true, expr = true, desc = "Scroll forward", mode = {"i", "n", "s"} },
-    { "<c-b>", function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end, silent = true, expr = true, desc = "Scroll backward", mode = {"i", "n", "s"}},
+    { "<S-Enter>",   function() require("noice").redirect(vim.fn.getcmdline()) end,                 mode = "c",                 desc = "Redirect Cmdline" },
+    { "<leader>pnl", function() require("noice").cmd("last") end,                                   desc = "Noice Last Message" },
+    { "<leader>pnh", function() require("noice").cmd("history") end,                                desc = "Noice History" },
+    { "<leader>pna", function() require("noice").cmd("all") end,                                    desc = "Noice All" },
+    { "<leader>pnd", function() require("noice").cmd("dismiss") end,                                desc = "Dismiss All" },
+    { "<c-f>",       function() if not require("noice.lsp").scroll(4) then return "<c-f>" end end,  silent = true,              expr = true,              desc = "Scroll forward",  mode = { "i", "n", "s" } },
+    { "<c-b>",       function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end, silent = true,              expr = true,              desc = "Scroll backward", mode = { "i", "n", "s" } },
   },
 }
