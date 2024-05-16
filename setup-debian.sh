d1cd1e176af4e23569f7548a022647b018e823fc
nlahmi <mogobekl123@gmail.com>
2024-05-16 20:34:16 +0300

    Added fonts to setup-debian.sh

--- setup-debian.sh
+++ setup-debian.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Install Fonts
+find fonts -type f -name "*.ttf" -exec cp "{}" ~/.local/share/fonts \;  # Consider changing to a simple recursive copy
+fc-cache -f -v
+
 # Install wezterm
 curl -fsSL https://apt.fury.io/wez/gpg.key | sudo gpg --yes --dearmor -o /usr/share/keyrings/wezterm-fury.gpg
 echo 'deb [signed-by=/usr/share/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | sudo tee /etc/apt/sources.list.d/wezterm.list
