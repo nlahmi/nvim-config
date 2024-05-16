4ff2ab0fd0cc476c7a8fef8bdf89fcebb6ab0430
nlahmi <mogobekl123@gmail.com>
2024-05-16 22:36:57 +0300

    Fixed python on linux

--- lua/utils.lua
+++ lua/utils.lua
@@ -1,84 +1,87 @@
 File_exists = function(name)
-    local f = io.open(name, "r")
-    if f ~= nil then
-        io.close(f)
-        return true
-    else
-        return false
-    end
+  local f = io.open(name, "r")
+  if f ~= nil then
+    io.close(f)
+    return true
+  else
+    return false
+  end
 end
 
 return {
 
-    get_py_exe = function(install_path)
-        if File_exists(install_path .. "/venv/bin/pythonw") then
-            return install_path .. "/venv/bin/pythonw"
-        end
-        return install_path .. "/venv/Scripts/pythonw"
-    end,
+  get_py_exe = function(install_path)
+    if File_exists(install_path .. "/venv/bin/python") then
+      -- print("bin/python")
+      return install_path .. "/venv/bin/python"
+    end
 
-    icons = {
-        misc = {
-            dots = "󰇘",
-        },
-        dap = {
-            Stopped = { "󰁕 ", "DiagnosticWarn", "DapStoppedLine" },
-            Breakpoint = " ",
-            BreakpointCondition = " ",
-            BreakpointRejected = { " ", "DiagnosticError" },
-            LogPoint = ".>",
-        },
-        diagnostics = {
-            Error = " ",
-            Warn = " ",
-            Hint = " ",
-            Info = " ",
-        },
-        git = {
-            added = " ",
-            modified = " ",
-            removed = " ",
-        },
-        kinds = {
-            Array = " ",
-            Boolean = "󰨙 ",
-            Class = " ",
-            Codeium = "󰘦 ",
-            Color = " ",
-            Control = " ",
-            Collapsed = " ",
-            Constant = "󰏿 ",
-            Constructor = " ",
-            Copilot = " ",
-            Enum = " ",
-            EnumMember = " ",
-            Event = " ",
-            Field = " ",
-            File = " ",
-            Folder = " ",
-            Function = "󰊕 ",
-            Interface = " ",
-            Key = " ",
-            Keyword = " ",
-            Method = "󰊕 ",
-            Module = " ",
-            Namespace = "󰦮 ",
-            Null = " ",
-            Number = "󰎠 ",
-            Object = " ",
-            Operator = " ",
-            Package = " ",
-            Property = " ",
-            Reference = " ",
-            Snippet = " ",
-            String = " ",
-            Struct = "󰆼 ",
-            TabNine = "󰏚 ",
-            Text = " ",
-            TypeParameter = " ",
-            Unit = " ",
-            Value = " ",
-            Variable = "󰀫 ",
-        },
+    -- print("Scripts/pythonw")
+    return install_path .. "/venv/Scripts/pythonw"
+  end,
+
+  icons = {
+    misc = {
+      dots = "󰇘",
     },
+    dap = {
+      Stopped = { "󰁕 ", "DiagnosticWarn", "DapStoppedLine" },
+      Breakpoint = " ",
+      BreakpointCondition = " ",
+      BreakpointRejected = { " ", "DiagnosticError" },
+      LogPoint = ".>",
+    },
+    diagnostics = {
+      Error = " ",
+      Warn = " ",
+      Hint = " ",
+      Info = " ",
+    },
+    git = {
+      added = " ",
+      modified = " ",
+      removed = " ",
+    },
+    kinds = {
+      Array = " ",
+      Boolean = "󰨙 ",
+      Class = " ",
+      Codeium = "󰘦 ",
+      Color = " ",
+      Control = " ",
+      Collapsed = " ",
+      Constant = "󰏿 ",
+      Constructor = " ",
+      Copilot = " ",
+      Enum = " ",
+      EnumMember = " ",
+      Event = " ",
+      Field = " ",
+      File = " ",
+      Folder = " ",
+      Function = "󰊕 ",
+      Interface = " ",
+      Key = " ",
+      Keyword = " ",
+      Method = "󰊕 ",
+      Module = " ",
+      Namespace = "󰦮 ",
+      Null = " ",
+      Number = "󰎠 ",
+      Object = " ",
+      Operator = " ",
+      Package = " ",
+      Property = " ",
+      Reference = " ",
+      Snippet = " ",
+      String = " ",
+      Struct = "󰆼 ",
+      TabNine = "󰏚 ",
+      Text = " ",
+      TypeParameter = " ",
+      Unit = " ",
+      Value = " ",
+      Variable = "󰀫 ",
+    },
+  },
 }
