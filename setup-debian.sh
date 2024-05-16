17c40876fe6c136da14d07b8b6439b39e594c333
nlahmi <mogobekl123@gmail.com>
2024-05-16 20:15:42 +0300

    Create setup-debian.sh

---
+++ setup-debian.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Install wezterm
+curl -fsSL https://apt.fury.io/wez/gpg.key | sudo gpg --yes --dearmor -o /usr/share/keyrings/wezterm-fury.gpg
+echo 'deb [signed-by=/usr/share/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | sudo tee /etc/apt/sources.list.d/wezterm.list
+
+sudo apt update
+sudo apt install wezterm ripgrep fd-find git python3 python3-venv python3-pip npm -y
+
+# Install from appimage since apt is always outdated
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod 755 nvim.appimage
+sudo chown root:root nvim.appimage
+sudo mv nvim.appimage /usr/local/bin/nvim
+
+git clone https://github.com/nlahmi/nvim-config ~/.config/nvim
+
+
+
+#LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+#curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+#tar xf lazygit.tar.gz lazygit
+#sudo install lazygit /usr/local/bin
