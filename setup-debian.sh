af081f7c09492bae5e72233c9b06f4be2fab668a
nlahmi <mogobekl123@gmail.com>
2024-05-20 12:35:09 +0300

    WSL1 Support and auto-install Mason+Lazy

--- setup-debian.sh
+++ setup-debian.sh
@@ -7,11 +7,12 @@ find ~/.config/nvim/fonts -type f -name "*.ttf" -exec cp "{}" ~/.local/share/fon
 fc-cache -f -v
 
 # Install wezterm
-curl -fsSL https://apt.fury.io/wez/gpg.key | sudo gpg --yes --dearmor -o /usr/share/keyrings/wezterm-fury.gpg
-echo 'deb [signed-by=/usr/share/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | sudo tee /etc/apt/sources.list.d/wezterm.list
+#curl -fsSL https://apt.fury.io/wez/gpg.key | sudo gpg --yes --dearmor -o /usr/share/keyrings/wezterm-fury.gpg
+#echo 'deb [signed-by=/usr/share/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | sudo tee /etc/apt/sources.list.d/wezterm.list
 
 sudo apt update
-sudo apt install wezterm ripgrep fd-find git python3 python3-venv python3-pip npm -y
+sudo apt install ripgrep fd-find git python3 python3-venv python3-pip npm -y 
+ # sudo apt install wezterm -y
 
 # Install from appimage since apt is always outdated
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
@@ -19,7 +20,19 @@ chmod 755 nvim.appimage
 sudo chown root:root nvim.appimage
 sudo mv nvim.appimage /usr/local/bin/nvim
 
+# On WSL1 (or any other system that doesnt have FUSE/Modprobe, we need to extract the Appimage
+if [ ! -f /dev/fuse ]; then
+    sudo mv /usr/local/bin/nvim /usr/local/bin/nvim.appimage
+    nvim.appimage --appimage-extract
+    sudo mv squashfs-root /opt/
+    sudo ln -s /opt/squashfs-root/AppRun /usr/bin/nvim
+fi
 
+# Install plugins with Lazy
+nvim --headless "+Lazy! install" +qa
+
+# Install dependencies with Mason
+nvim "+Mason"
 
 #LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 #curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
