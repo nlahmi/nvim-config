dd7212621526241badfda390c97600136569021c
nlahmi <mogobekl123@gmail.com>
2024-07-06 01:00:51 +0300

    Added treesitter plugins for easier navigation

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,17 +1,86 @@
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-        config = function()
-            require("nvim-treesitter.configs").setup({
-                auto_install = true,
-                ignore_install = { "yaml", },  -- Fix attempt for yaml issues
-                highlight = { enable = true },
-                indent = { enable = true },
-            })
-        end,
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        auto_install = true,
+        ignore_install = {
+          -- "yaml", -- Fix attempt for yaml issues
+        },
+        highlight = { enable = true },
+        indent = { enable = true },
+
+        -- textsubjects
+        textsubjects = {
+          enable = true,
+          prev_selection = ",", -- (Optional) keymap to select the previous selection
+          keymaps = {
+            ["."] = "textsubjects-smart",
+            [";"] = "textsubjects-container-outer",
+            ["i;"] = { "textsubjects-container-inner", desc = "Select inside containers (classes, functions, etc.)" },
+          },
+        },
+
+        -- This next bit (textobjects) was copied from https://github.com/omerxx/dotfiles/blob/master/nvim/lua/plugins/treesitter.lua
+        textobjects = {
+          select = {
+            enable = true,
+            lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+
+            keymaps = {
+              -- You can use the capture groups defined in textobjects.scm
+              ["aa"] = "@parameter.outer",
+              ["ia"] = "@parameter.inner",
+              ["af"] = "@function.outer",
+              ["if"] = "@function.inner",
+              ["ac"] = "@class.outer",
+              ["ic"] = "@class.inner",
+              ["ii"] = "@conditional.inner",
+              ["ai"] = "@conditional.outer",
+              ["il"] = "@loop.inner",
+              ["al"] = "@loop.outer",
+              ["at"] = "@comment.outer",
+            },
+          },
+          move = {
+            enable = true,
+            set_jumps = true, -- whether to set jumps in the jumplist
+            goto_next_start = {
+              ["]f"] = "@function.outer",
+              ["]]"] = "@class.outer",
+            },
+            goto_next_end = {
+              ["]F"] = "@function.outer",
+              ["]["] = "@class.outer",
+            },
+            goto_previous_start = {
+              ["[f"] = "@function.outer",
+              ["[["] = "@class.outer",
+            },
+            goto_previous_end = {
+              ["[F"] = "@function.outer",
+              ["[]"] = "@class.outer",
+            },
+          },
+          swap = {
+            enable = true,
+            swap_next = {
+              ["<leader>a"] = "@parameter.inner",
+            },
+            swap_previous = {
+              ["<leader>A"] = "@parameter.inner",
+            },
+          },
+        },
+      })
+    end,
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+      "RRethy/nvim-treesitter-textsubjects",
     },
-    {
-        "nvim-treesitter/nvim-treesitter-context",
-    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+  },
 }
