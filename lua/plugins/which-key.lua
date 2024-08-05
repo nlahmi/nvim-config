34b90175fd514a56e47cada0e7e626a42ed1a45e
nlahmi <mogobekl123@gmail.com>
2024-08-05 13:12:11 +0300

    Updated whichkey config to new format

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -4,33 +4,36 @@ return {
     opts = {
         plugins = { spelling = true },
         defaults = {
-            mode = { "n", "v" },
-            ["g"] = { name = "+goto" },
-            ["gs"] = { name = "+surround" },
-            ["]"] = { name = "+next" },
-            ["["] = { name = "+prev" },
-            ["<leader><tab>"] = { name = "+tabs" },
-            ["<leader>b"] = { name = "+buffer" },
-            ["<leader>c"] = { name = "+code" },
-            ["<leader>cw"] = { name = "+workspace" },
-            ["<leader>d"] = { name = "+debug" },
-            ["<leader>f"] = { name = "+file/find" },
-            ["<leader>g"] = { name = "+git" },
-            ["<leader>gh"] = { name = "+hunks" },
-            ["<leader>p"] = { name = "+plugins" },
-            ["<leader>pn"] = { name = "+noice" },
-            ["<leader>q"] = { name = "+quit/session" },
-            ["<leader>r"] = { name = "+run" },
-            ["<leader>s"] = { name = "+search" },
-            ["<leader>u"] = { name = "+ui" },
-            ["<leader>w"] = { name = "+windows" },
-            ["<leader>x"] = { name = "+diagnostics/quickfix" },
-        },
+            -- mode = { "n", "v" },
+        }
     },
     config = function(_, opts)
         local wk = require("which-key")
         wk.setup(opts)
         wk.register(opts.defaults)
+
+        wk.add({
+            { "g", desc = "+goto" },
+            { "gs", name = "+surround" },
+            { "]", desc = "+next" },
+            { "[", desc = "+prev" },
+            { "<leader><tab>", desc = "+tabs" },
+            { "<leader>b", desc = "+buffer" },
+            { "<leader>c", desc = "+code" },
+            { "<leader>cw", desc = "+workspace" },
+            { "<leader>d", desc = "+debug" },
+            { "<leader>f", desc = "+file/find" },
+            { "<leader>g", desc = "+git" },
+            { "<leader>gh", desc = "+hunks" },
+            { "<leader>p", desc = "+plugins" },
+            { "<leader>pn", desc = "+noice" },
+            { "<leader>q", desc = "+quit/session" },
+            { "<leader>r", desc = "+run" },
+            { "<leader>s", desc = "+search" },
+            { "<leader>u", desc = "+ui" },
+            { "<leader>w", desc = "+windows" },
+            { "<leader>x", desc = "+diagnostics/quickfix" }
+        })
     end,
     init = function()
         vim.o.timeout = true
