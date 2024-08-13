e0f8195d43f21da7fdb4b216e888f64161f3e2a1
nlahmi <mogobekl123@gmail.com>
2024-08-13 10:57:39 +0300

    Set oil to not ask confirmation for simple operations

--- lua/plugins/ui/oil.lua
+++ lua/plugins/ui/oil.lua
@@ -5,6 +5,7 @@ return {
       -- buf_options = {
       --   buf_hidden = "show",
       -- },
+      skip_confirm_for_simple_edits = true,
       watch_for_changes = true,
       view_options = {
         show_hidden = true,
