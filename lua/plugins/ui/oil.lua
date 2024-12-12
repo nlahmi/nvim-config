95ee8c02375ebb2c33d88f3d638ff537aa28c5ab
nlahmi <mogobekl123@gmail.com>
2024-12-12 13:44:27 +0200

    Added PWD print to oil

--- lua/plugins/ui/oil.lua
+++ lua/plugins/ui/oil.lua
@@ -1,10 +1,24 @@
 return {
   "stevearc/oil.nvim",
   config = function()
+    -- Declare a global function to retrieve the current directory
+    function _G.get_oil_winbar()
+      local dir = require("oil").get_current_dir()
+      if dir then
+        return vim.fn.fnamemodify(dir, ":~")
+      else
+        -- If there is no current directory (e.g. over ssh), just show the buffer name
+        return vim.api.nvim_buf_get_name(0)
+      end
+    end
+
     require("oil").setup({
       -- buf_options = {
       --   buf_hidden = "show",
       -- },
+      win_options = {
+        winbar = "%!v:lua.get_oil_winbar()",
+      },
       skip_confirm_for_simple_edits = true,
       watch_for_changes = true,
       view_options = {
