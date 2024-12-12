c5dffab4c7b64c2e78a64c4054aafb02274052f8
nlahmi <mogobekl123@gmail.com>
2024-12-12 15:41:22 +0200

    Minor improvements to DAP

--- lua/plugins/lsp/dap.lua
+++ lua/plugins/lsp/dap.lua
@@ -79,14 +79,15 @@ return {
       end,
       desc = "Run Last"
     },
-    -- { "<leader>dd", function() vim.api.nvim_input("<leader>dl") end,          desc = "Run Last" },
+    -- { "<leader>dd", function() vim.api.nvim_input("<leader>dl") end, desc = "Run Last" },
     { "<leader>do", function() require("dap").step_out() end,                                                 desc = "Step Out" },
     { "<leader>dO", function() require("dap").step_over() end,                                                desc = "Step Over" },
     { "<leader>dp", function() require("dap").pause() end,                                                    desc = "Pause" },
-    { "<leader>dr", function() require("dap").repl.toggle(nil, "vsplit") end,                                 desc = "Toggle REPL" },
+    { "<leader>dr", function() require("dap").repl.toggle(nil, "belowright vsplit") end,                      desc = "Toggle REPL" },
     { "<leader>ds", function() require("dap").session() end,                                                  desc = "Session" },
     { "<leader>dt", function() require("dap").terminate() end,                                                desc = "Terminate" },
-    { "<leader>dw", function() require("dap.ui.widgets").hover() end,                                         desc = "Widgets" },
+    { "<leader>dw", function() require("dap.ui.widgets").hover() end,                                         desc = "Debug Hover" },
+    -- TODO: re-use the same load_launchjs as above
     { "<leader>dv", function() require("dap.ext.vscode").load_launchjs(nil, { cppdbg = { "c", "cpp" } }) end, desc = "Reload .vscode" },
   },
 
