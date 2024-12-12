1b429983f0b96e7e7a12b607b8d4d811d0fe62c3
n.lahmi <mogobekl123@gmail.com>
2024-12-13 00:06:43 +0200

    Replaced the deprecated neodev with lazydev

--- lua/plugins/lsp/lang/lua.lua
+++ lua/plugins/lsp/lang/lua.lua
@@ -1,44 +1,55 @@
 return {
-    packages = {
-        { "folke/neodev.nvim", opts = {} },
+  packages = {
+    {
+      "folke/lazydev.nvim",
+      ft = "lua", -- only load on lua files
+      opts = {
+        library = {
+          -- See the configuration section for more details
+          -- Load luvit types when the `vim.uv` word is found
+          { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+        },
+      },
     },
+    -- { "folke/neodev.nvim", opts = {} },
+  },
 
-    mason_packages = {
-        "lua-language-server",
-        "stylua",
-    },
+  mason_packages = {
+    "lua-language-server",
+    "stylua",
+  },
 
-    nonls_packages = {},
+  nonls_packages = {},
 
-    lsp_config = {
-        function(lspconfig, capabilities, custom_attach)
-            lspconfig.lua_ls.setup({
-                on_attach = custom_attach,
-                settings = {
-                    Lua = {
-                        runtime = {
-                            -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-                            version = "LuaJIT",
-                        },
-                        diagnostics = {
-                            -- Get the language server to recognize the `vim` global
-                            globals = { "vim" },
-                        },
-                        workspace = {
-                            -- Make the server aware of Neovim runtime files,
-                            -- see also https://github.com/LuaLS/lua-language-server/wiki/Libraries#link-to-workspace .
-                            -- Lua-dev.nvim also has similar settings for lua ls, https://github.com/folke/neodev.nvim/blob/main/lua/neodev/luals.lua .
-                            --library = {
-                            --  fn.stdpath("data") .. "/lazy/emmylua-nvim",
-                            --  fn.stdpath("config"),
-                            --},
-                            maxPreload = 2000,
-                            preloadFileSize = 50000,
-                        },
-                    },
-                },
-                capabilities = capabilities,
-            })
-        end,
-    },
+  lsp_config = {
+    function(lspconfig, capabilities, custom_attach)
+      lspconfig.lua_ls.setup({
+        on_attach = custom_attach,
+        settings = {
+          Lua = {
+            runtime = {
+              -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+              version = "LuaJIT",
+            },
+            diagnostics = {
+              -- Get the language server to recognize the `vim` global
+              globals = { "vim" },
+            },
+            workspace = {
+              -- Make the server aware of Neovim runtime files,
+              -- see also https://github.com/LuaLS/lua-language-server/wiki/Libraries#link-to-workspace .
+              -- Lua-dev.nvim also has similar settings for lua ls, https://github.com/folke/neodev.nvim/blob/main/lua/neodev/luals.lua .
+              --library = {
+              --  fn.stdpath("data") .. "/lazy/emmylua-nvim",
+              --  fn.stdpath("config"),
+              --},
+              maxPreload = 2000,
+              preloadFileSize = 50000,
+            },
+          },
+        },
+        capabilities = capabilities,
+      })
+    end,
+  },
 }
