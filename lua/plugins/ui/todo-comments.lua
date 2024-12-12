86522642eef08b21d949944692874c9afc5cba2c
nlahmi <mogobekl123@gmail.com>
2024-12-12 13:43:07 +0200

    Changed bookmark keymap name

--- lua/plugins/ui/todo-comments.lua
+++ lua/plugins/ui/todo-comments.lua
@@ -10,7 +10,7 @@ return {
   keys = {
     { "<C-t>", "<cmd>TodoTelescope<cr>", desc = "Todo Telescope" },
     { "<leader>xt", "<cmd>TodoTelescope<cr>", desc = "Todo Telescope" },
-    { "<leader>xm", "<cmd>TodoTelescope keywords=MARK,BM,BOOKMARK<cr>", desc = "Todo Telescope" },
+    { "<leader>xm", "<cmd>TodoTelescope keywords=MARK,BM,BOOKMARK<cr>", desc = "Todo Bookmarks Telescope" },
     { "<leader>xM", function() require("Comment.api").insert.linewise.above(); vim.api.nvim_feedkeys("MARK: ", "", false) end, desc = "Insert Mark above" },
   },
 }
