57f36d66818a556580ed6b64d501c8fd5df57909
nlahmi <mogobekl123@gmail.com>
2024-12-22 13:49:43 +0200

    dap-repl and qf exception from buffer tab line

--- lua/plugins/ui/bufferline.lua
+++ lua/plugins/ui/bufferline.lua
@@ -1,54 +1,63 @@
-return
-  -- This is what powers LazyVim's fancy-looking
-  -- tabs, which include filetype icons and close buttons.
-  {
-    "akinsho/bufferline.nvim",
-    event = "VeryLazy",
-    keys = {
-      { "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle pin" },
-      { "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete non-pinned buffers" },
-      { "<leader>bo", "<Cmd>BufferLineCloseOthers<CR>", desc = "Delete other buffers" },
-      { "<leader>br", "<Cmd>BufferLineCloseRight<CR>", desc = "Delete buffers to the right" },
-      { "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>", desc = "Delete buffers to the left" },
-      { "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev buffer" },
-      { "<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
-      { "[b", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev buffer" },
-      { "]b", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
-    },
-    opts = {
-      options = {
-        -- stylua: ignore
-        close_command = function(n) require("mini.bufremove").delete(n, false) end,
-        -- stylua: ignore
-        --right_mouse_command = function(n) require("mini.bufremove").delete(n, false) end,
-        -- TODO: Add exceptions for utility buffers such as DAP and qf
-        diagnostics = "nvim_lsp",
-        always_show_bufferline = true,
-        diagnostics_indicator = function(_, _, diag)
-         local icons = require("utils").icons.diagnostics
-         local ret = (diag.error and icons.Error .. diag.error .. " " or "")
-           .. (diag.warning and icons.Warn .. diag.warning or "")
-         return vim.trim(ret)
-        end,
-        offsets = {
-         {
-           filetype = "neo-tree",
-           text = "Neo-tree",
-           highlight = "Directory",
-           text_align = "left",
-         },
+-- This is what powers LazyVim's fancy-looking
+-- tabs, which include filetype icons and close buttons.
+return {
+  "akinsho/bufferline.nvim",
+  event = "VeryLazy",
+  keys = {
+    { "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle pin" },
+    { "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete non-pinned buffers" },
+    { "<leader>bo", "<Cmd>BufferLineCloseOthers<CR>", desc = "Delete other buffers" },
+    { "<leader>br", "<Cmd>BufferLineCloseRight<CR>", desc = "Delete buffers to the right" },
+    { "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>", desc = "Delete buffers to the left" },
+    { "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev buffer" },
+    { "<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
+    { "[b", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev buffer" },
+    { "]b", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
+  },
+  opts = {
+    options = {
+      close_command = function(n)
+        require("mini.bufremove").delete(n, false)
+      end,
+      -- right_mouse_command = function(n) require("mini.bufremove").delete(n, false) end,
+      diagnostics = "nvim_lsp",
+      always_show_bufferline = true,
+      diagnostics_indicator = function(_, _, diag)
+        local icons = require("utils").icons.diagnostics
+        local ret = (diag.error and icons.Error .. diag.error .. " " or "")
+          .. (diag.warning and icons.Warn .. diag.warning or "")
+        return vim.trim(ret)
+      end,
+      offsets = {
+        {
+          filetype = "neo-tree",
+          text = "Neo-tree",
+          highlight = "Directory",
+          text_align = "left",
         },
       },
+      custom_filter = function(buf, buf_nums)
+        -- local ignored_buffers = {"dap-repl", "qf"}
+        -- NOTE: use `print(vim.bo.filetype)` to figure out the buffer type
+
+        -- Don't show DAP Repl or Quickfix buffers
+        if vim.bo[buf].filetype == "dap-repl" or vim.bo[buf].filetype == "dap-repl" then
+          return false
+        end
+
+        return true
+      end,
     },
-    config = function(_, opts)
-      local nvim_bufferline = require("bufferline").setup(opts)
-      -- Fix bufferline when restoring a session
-      vim.api.nvim_create_autocmd("BufAdd", {
-        callback = function()
-          vim.schedule(function()
-            pcall(nvim_bufferline)
-          end)
-        end,
-      })
-    end,
-  }
+  },
+  config = function(_, opts)
+    local nvim_bufferline = require("bufferline").setup(opts)
+    -- Fix bufferline when restoring a session
+    vim.api.nvim_create_autocmd("BufAdd", {
+      callback = function()
+        vim.schedule(function()
+          pcall(nvim_bufferline)
+        end)
+      end,
+    })
+  end,
+}
