588401c7fcf4751cb36fccb2c5cace8cf75539d4
nlahmi <mogobekl123@gmail.com>
2025-01-07 14:25:39 +0200

    Fixed dap duplicate entries

--- lua/plugins/lsp/dap.lua
+++ lua/plugins/lsp/dap.lua
@@ -57,27 +57,13 @@ return {
     { "<leader>dR", function() require("persistent-breakpoints.api").clear_all_breakpoints() end,      desc = "Breakpoint Condition" },
     { "<leader>dB", function() require("persistent-breakpoints.api").set_conditional_breakpoint() end, desc = "Breakpoint Condition" },
     { "<leader>db", function() require("persistent-breakpoints.api").toggle_breakpoint() end,          desc = "Toggle Breakpoint" },
-    {
-      "<leader>dc",
-      function()
-        -- (Re-)reads launch.json if present
-        -- if vim.fn.filereadable(".vscode/launch.json") then
-        -- require("dap.ext.vscode").load_launchjs(nil, {
-        --   gdb = { "c", "cpp" },
-        --   debugpy = { "python" },
-        -- })
-        -- end
-        reload_vscode_configs()
-        require("dap").continue()
-      end,
-      desc = "Continue"
-    },
-    { "<leader>da", function() require("dap").continue({ before = get_args }) end, desc = "Run with Args" },
-    { "<leader>dC", function() require("dap").run_to_cursor() end,                 desc = "Run to Cursor" },
-    { "<leader>dg", function() require("dap").goto_() end,                         desc = "Go to line (no execute)" },
-    { "<leader>di", function() require("dap").step_into() end,                     desc = "Step Into" },
-    { "<leader>dj", function() require("dap").down() end,                          desc = "Down" },
-    { "<leader>dk", function() require("dap").up() end,                            desc = "Up" },
+    { "<leader>dc", function() require("dap").continue() end,                                          desc = "Continue" },
+    { "<leader>da", function() require("dap").continue({ before = get_args }) end,                     desc = "Run with Args" },
+    { "<leader>dC", function() require("dap").run_to_cursor() end,                                     desc = "Run to Cursor" },
+    { "<leader>dg", function() require("dap").goto_() end,                                             desc = "Go to line (no execute)" },
+    { "<leader>di", function() require("dap").step_into() end,                                         desc = "Step Into" },
+    { "<leader>dj", function() require("dap").down() end,                                              desc = "Down" },
+    { "<leader>dk", function() require("dap").up() end,                                                desc = "Up" },
     {
       "<leader>dl",
       function()
