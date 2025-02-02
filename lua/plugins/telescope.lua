31511f1525c8639570dd42fd6628d2630b6de7a9
nlahmi <mogobekl123@gmail.com>
2025-02-02 17:51:48 +0200

    Added filesize limits to telescope grep

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -8,6 +8,12 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
       require("telescope").setup({
+        defaults = {
+          preview = {
+            filesize_limit = 0.9999,
+            timeout = 250,
+          },
+        },
         extensions = {
           ["ui-select"] = {
             require("telescope.themes").get_dropdown({}),
