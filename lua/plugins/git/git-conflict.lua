8bd80635e6b86712b4c58493f143bcd477a35d01
nlahmi <mogobekl123@gmail.com>
2025-03-25 18:02:13 +0200

    Show conflict list (half working)

--- lua/plugins/git/git-conflict.lua
+++ lua/plugins/git/git-conflict.lua
@@ -3,4 +3,8 @@ return {
   version = "*",
   config = true,
   -- TODO: Consider remapping default keys
+  --
+  keys = {
+    { "<leader>gC", "<Cmd>GitConflictListQf<CR>", desc = "Show Conflicts (qf)" },
+  },
 }
