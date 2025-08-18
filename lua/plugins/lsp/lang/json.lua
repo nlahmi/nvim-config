578eb86a9d7ad2c63ce6bb86a27c8d1b4aacb122
n.lahmi <mogobekl123@gmail.com>
2025-08-18 11:29:43 +0300

    Added json tools

--- lua/plugins/lsp/lang/json.lua
+++ lua/plugins/lsp/lang/json.lua
@@ -1,29 +1,44 @@
-
 return {
-    packages = {
-        { "ikatyang/tree-sitter-yaml" },
-        { "b0o/SchemaStore.nvim" },
+  packages = {
+    { "ikatyang/tree-sitter-yaml" },
+    { "b0o/SchemaStore.nvim" },
+    {
+      "https://git.myzel394.app/Myzel394/jsonfly.nvim",
+      dependencies = {
+        "nvim-telescope/telescope.nvim",
+        "phelipetls/jsonpath.nvim",
+      },
+      keys = {
+        {
+          "<leader>fj",
+          "<cmd>Telescope jsonfly<cr>",
+          desc = "Open json(fly)",
+          ft = { "json", "xml", "yaml" },
+          mode = "n",
+        },
+      },
     },
+  },
 
-    mason_packages = {
-        "json-lsp",
-    },
+  mason_packages = {
+    "json-lsp",
+  },
 
-    nonls_packages = {},
+  nonls_packages = {},
 
-    lsp_config = {
+  lsp_config = {
 
-        function(lspconfig, capabilities, custom_attach)
-            lspconfig.jsonls.setup({
-                on_attach = custom_attach,
-                capabilities = capabilities,
-                settings = {
-                    json = {
-                        schemas = require("schemastore").json.schemas(),
-                        validate = { enable = true },
-                    },
-                },
-            })
-        end,
-    },
+    function(lspconfig, capabilities, custom_attach)
+      lspconfig.jsonls.setup({
+        on_attach = custom_attach,
+        capabilities = capabilities,
+        settings = {
+          json = {
+            schemas = require("schemastore").json.schemas(),
+            validate = { enable = true },
+          },
+        },
+      })
+    end,
+  },
 }
