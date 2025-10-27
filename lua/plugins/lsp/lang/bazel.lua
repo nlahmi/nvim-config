e40380f6d24c69f2c2b734d75260a6424016bea7
n.lahmi <mogobekl123@gmail.com>
2025-10-27 17:48:55 +0200

    Somewhat working bazel LSP

--- lua/plugins/lsp/lang/bazel.lua
+++ lua/plugins/lsp/lang/bazel.lua
@@ -1,27 +1,35 @@
 return {
   packages = {
-    {
-      "alexander-born/bazel.nvim",
-      dependencies = {
-        -- { "nvim-treesitter/nvim-treesitter" },
-        -- { "nvim-lua/plenary.nvim" },
-        -- { "bazelbuild/vim-bazel", dependencies = { "google/vim-maktaba" } },
-        {
-          "saghen/blink.cmp",
-          dependencies = { "saghen/blink.compat", { "alexander-born/cmp-bazel", dependencies = "hrsh7th/nvim-cmp" } },
-          opts = {
-            sources = {
-              compat = { "bazel" },
-            },
-          },
-        },
-      },
-    },
+    -- {
+    --   "alexander-born/bazel.nvim",
+    --   dependencies = {
+    --     -- { "nvim-treesitter/nvim-treesitter" },
+    --     -- { "nvim-lua/plenary.nvim" },
+    --     -- { "bazelbuild/vim-bazel", dependencies = { "google/vim-maktaba" } },
+    --     {
+    --       "saghen/blink.cmp",
+    --       dependencies = { "saghen/blink.compat", { "alexander-born/cmp-bazel", dependencies = "hrsh7th/nvim-cmp" } },
+    --       opts = {
+    --         sources = {
+    --           compat = { "bazel" },
+    --         },
+    --       },
+    --     },
+    --   },
+    -- },
   },
 
-  mason_packages = {},
+  mason_packages = { "starpls" },
 
   nonls_packages = {},
 
-  lsp_config = {},
+  lsp_config = {
+    function(_, capabilities, custom_attach)
+      vim.lsp.config("starpls", {
+        capabilities = capabilities,
+        on_attach = custom_attach,
+        filetypes = { "bzl", "BUILD" },
+      })
+    end,
+  },
 }
