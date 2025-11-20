7ee97ea6918a294837008d72091408ae0ae2a50d
n.lahmi <mogobekl123@gmail.com>
2025-11-20 16:27:06 +0200

    format

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -14,13 +14,13 @@ return {
       local finder = function()
         local paths = {}
         for _, item in ipairs(harpoon_files.items) do
-            table.insert(paths, item.value)
+          table.insert(paths, item.value)
         end
 
         return require("telescope.finders").new_table({
-            results = paths,
+          results = paths,
         })
-    end
+      end
 
       local file_paths = {}
       for _, item in ipairs(harpoon_files.items) do
@@ -35,24 +35,24 @@ return {
           sorter = conf.generic_sorter({}),
           attach_mappings = function(prompt_bufnr, map)
             map("i", "<C-d>", function()
-                local state = require("telescope.actions.state")
-                local selected_entry = state.get_selected_entry()
-                local current_picker = state.get_current_picker(prompt_bufnr)
+              local state = require("telescope.actions.state")
+              local selected_entry = state.get_selected_entry()
+              local current_picker = state.get_current_picker(prompt_bufnr)
 
-                table.remove(harpoon_files.items, selected_entry.index)
-                current_picker:refresh(finder())
+              table.remove(harpoon_files.items, selected_entry.index)
+              current_picker:refresh(finder())
             end)
             return true
-        end,
+          end,
         })
         :find()
     end
 
-        -- stylua: ignore start
-        vim.keymap.set("n", "<S-Up>", function() toggle_telescope(harpoon:list()) end, { desc = "Open harpoon window" })
-        vim.keymap.set("n", "<S-Down>", function() harpoon:list():add() end)
-        vim.keymap.set("n", "<S-Left>", function() harpoon:list():prev() end)
-        vim.keymap.set("n", "<S-Right>", function() harpoon:list():next() end)
-        -- stylua: ignore end
+    -- stylua: ignore start
+    vim.keymap.set("n", "<S-Up>", function() toggle_telescope(harpoon:list()) end, { desc = "Open harpoon window" })
+    vim.keymap.set("n", "<S-Down>", function() harpoon:list():add() end)
+    vim.keymap.set("n", "<S-Left>", function() harpoon:list():prev() end)
+    vim.keymap.set("n", "<S-Right>", function() harpoon:list():next() end)
+    -- stylua: ignore end
   end,
 }
