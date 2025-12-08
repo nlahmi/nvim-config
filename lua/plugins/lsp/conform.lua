ca3d47b07844c1fed9b4b5df2fb780790b7f9597
n.lahmi <mogobekl123@gmail.com>
2025-12-08 14:30:54 +0200

    make conform the default formatter

--- lua/plugins/lsp/conform.lua
+++ lua/plugins/lsp/conform.lua
@@ -5,7 +5,7 @@ return {
   keys = {
     {
       -- Customize or remove this keymap to your liking
-      "<leader>cF",
+      "<leader>cf",
       function()
         require("conform").format({ async = true })
       end,
