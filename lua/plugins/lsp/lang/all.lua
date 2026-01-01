a2de86b8a1f74a55d075d73b1c3061ab6af2ff8a
n.lahmi <mogobekl123@gmail.com>
2026-01-01 14:23:42 +0200

    Some more cleanup

--- lua/plugins/lsp/lang/all.lua
+++ lua/plugins/lsp/lang/all.lua
@@ -1,40 +1,38 @@
 local all_langs = {
-    require("plugins.lsp.lang.python"),
-    require("plugins.lsp.lang.lua"),
-    require("plugins.lsp.lang.markdown"),
-    require("plugins.lsp.lang.java"),
-    require("plugins.lsp.lang.clangd"),
-    require("plugins.lsp.lang.yaml"),
-    require("plugins.lsp.lang.json"),
-    require("plugins.lsp.lang.spellcheck"),
-    require("plugins.lsp.lang.javascript"),
-    require("plugins.lsp.lang.ansible"),
-    require("plugins.lsp.lang.dotnet"),
-    require("plugins.lsp.lang.go"),
-    require("plugins.lsp.lang.cmake"),
-    require("plugins.lsp.lang.powershell"),
-    require("plugins.lsp.lang.bazel"),
-    require("plugins.lsp.lang.sh"),
-    require("plugins.lsp.lang.awk"),
-    require("plugins.lsp.lang.protobuf"),
+  require("plugins.lsp.lang.python"),
+  require("plugins.lsp.lang.lua"),
+  require("plugins.lsp.lang.markdown"),
+  require("plugins.lsp.lang.java"),
+  require("plugins.lsp.lang.clangd"),
+  require("plugins.lsp.lang.yaml"),
+  require("plugins.lsp.lang.json"),
+  require("plugins.lsp.lang.spellcheck"),
+  require("plugins.lsp.lang.javascript"),
+  require("plugins.lsp.lang.ansible"),
+  require("plugins.lsp.lang.dotnet"),
+  require("plugins.lsp.lang.go"),
+  require("plugins.lsp.lang.cmake"),
+  require("plugins.lsp.lang.powershell"),
+  require("plugins.lsp.lang.bazel"),
+  require("plugins.lsp.lang.sh"),
+  require("plugins.lsp.lang.awk"),
+  require("plugins.lsp.lang.protobuf"),
 }
 
 local out = {
-    packages = {},
-    mason_packages = {},
-    nonls_packages = {},
-    lsp_config = {},
-    dap_config = {},
+  packages = {},
+  mason_packages = {},
+  nonls_packages = {},
+  lsp_config = {},
+  dap_config = {},
 }
 
 for _, curr_lang in ipairs(all_langs) do
-    for key, v in pairs(out) do
-        if curr_lang[key] ~= nil then
-            vim.list_extend(v, curr_lang[key])
-        end
+  for key, v in pairs(out) do
+    if curr_lang[key] ~= nil then
+      vim.list_extend(v, curr_lang[key])
     end
+  end
 end
-
--- vim.print(out)
 
 return out
