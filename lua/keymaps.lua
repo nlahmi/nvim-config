56eaa90c132f0f66db1f17b2b0833dad0b533eac
n.lahmi <mogobekl123@gmail.com>
2026-01-05 17:37:37 +0200

    Improved other keys as well

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -183,11 +183,11 @@ vim.keymap.set("n", "<leader><tab>[", "<cmd>tabprevious<cr>", { desc = "Previous
 
 -- run
 -- Todo: move this to it's lsp config and specify ft
-vim.keymap.set("n", "<leader>rk", "<cmd>!kubectl apply -f %<cr>", { desc = "Kubectl Apply Current File" })
-vim.keymap.set("n", "<leader>rc", "<cmd>!chezmoi apply<cr>", { desc = "Chezmoi Apply" })
+vim.keymap.set("n", "<leader>rk", "<cmd>redir @z<cr><cmd>!kubectl apply -f %<cr><cmd>redir END<cr><cmd>reg z<cr>", { desc = "Kubectl Apply Current File" })
+vim.keymap.set("n", "<leader>rc", "<cmd>redir @z<cr><cmd>!chezmoi apply<cr><cmd>redir END<cr><cmd>reg z<cr>", { desc = "Chezmoi Apply" })
 
-vim.keymap.set("n", "<leader>rf", "<cmd>w<cr><cmd>!acli mani-pedi --file %<cr>", { desc = "ACLI Mani Pedi (current file)" })
-vim.keymap.set("n", "<leader>rF", "<cmd>wa<cr><cmd>!acli mani-pedi<cr>", { desc = "ACLI Mani Pedi (entire project)" })
+vim.keymap.set("n", "<leader>rf", "<cmd>redir @z<cr><cmd>!acli mani-pedi --file %<cr><cmd>redir END<cr><cmd>reg z<cr>", { desc = "ACLI Mani Pedi (current file)" })
+vim.keymap.set("n", "<leader>rF", "<cmd>redir @z<cr><cmd>!acli mani-pedi<cr><cmd>redir END<cr><cmd>reg z<cr>", { desc = "ACLI Mani Pedi (entire project)" })
 
 
 vim.keymap.set("n", "<leader>fp", "<cmd>lcd%:p:h<cr>", { desc = "PWD to Current File" })
