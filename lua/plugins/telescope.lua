aa824e2b99dfd2fc01e067b50c739d39e46d6411
n.lahmi <mogobekl123@gmail.com>
2026-01-11 11:50:36 +0200

    Update telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,7 +4,7 @@ return {
   },
   {
     "nvim-telescope/telescope.nvim",
-    tag = "0.1.8",
+    tag = "v0.2.1",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
       require("telescope").setup({
@@ -12,6 +12,8 @@ return {
           preview = {
             filesize_limit = 10.9999,
             timeout = 250,
+            -- Disable tree-sitter in telescope for performance
+            -- treesitter = false
           },
         },
         extensions = {
@@ -22,12 +24,14 @@ return {
       })
       local builtin = require("telescope.builtin")
       vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = "Find Files" })
-      vim.keymap.set("n", "<C-b>", builtin.buffers, { desc = "Buffers" })
+      -- vim.keymap.set("n", "<C-b>", builtin.buffers, { desc = "Buffers" })
       vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Buffers" })
       vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Grep in Files" })
+      -- vim.keymap.set("n", "<leader>fg", function() builtin.live_grep({ debounce = 100 }) end, { desc = "Grep in Files" })
       vim.keymap.set("n", "<leader>fG", builtin.grep_string, { desc = "Grep in Files (Current Directory)" })
       vim.keymap.set("n", "<leader>fr", builtin.resume, { desc = "Resume Telescope" })
       vim.keymap.set("n", "<leader>fR", builtin.pickers, { desc = "Resume Telescope (Pickers)" })
+      -- vim.keymap.set("n", "?", builtin.current_buffer_fuzzy_find, { desc = "Resume Telescope (Pickers)" })
 
       require("telescope").load_extension("ui-select")
     end,
